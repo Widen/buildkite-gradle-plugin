3b46700e79911553cd917326187897d20874fd27
Dan Beghin <dbeg@users.noreply.github.com>
2024-09-25 16:49:36 -0500

    fix(plugin): publishing (#9)

---
+++ buildSrc/src/main/java/com/widen/plugins/buildkite/JavadocStub.java
@@ -0,0 +1,8 @@
+package com.widen.plugins.buildkite;
+
+/**
+ * This class only serves as a placeholder to generate non-empty javadoc, which appears to be required for
+ * plugin publishing (and having multiple sources with the same hash causes an error within the plugin-publish plugin).
+ */
+public class JavadocStub {
+}
